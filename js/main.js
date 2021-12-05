a09095b16a89dd3a9a6debd118720f5e0db9ff0e
ravshan oblayorxonov <ravshankhan777@icloud.com>
2021-12-06 00:35:48 +0500

    tugadi.

--- js/main.js
+++ js/main.js
@@ -8,132 +8,64 @@ let elBtnFriends = document.getElementById("btn-friends");
 let elBtnClassm = document.getElementById("btn-classmates");
 let elBtnAll = document.getElementById("btn-all");
 let elContactArea = document.querySelector(".contact-area");
-let contactArr = [];
+let arr = [];
+let template = document.querySelector("#template").content;
 
 elAddBtn.addEventListener("click", addContact)
 function addContact(e){
   e.preventDefault();
   if(elInputName.value != "" && elInputNumber.value != "" && elInputRel.value != ""){
-    let contactInfo = {
-      Name : elInputName.value,
-      LName : elInputLName.value,
-      rel : elInputRel.value,
-      tel : elInputNumber.value
+    let obj = {
+      fullName: elInputName.value + " " + elInputLName.value,
+      rel: elInputRel.value,
+      num: elInputNumber.value
     };
-    contactArr.push(contactInfo)
-    // create element
-    let elContact = document.createElement("div");
-    let elContactTitle = document.createElement("h4");
-    let elContactText = document.createElement("p");
-    let elContactTel = document.createElement("a");
-    // gettting texts to element
-    elContact.className = "contact p-3 border border-start rounded";
-    elContactTitle.className = "contact__title";
-    elContactText.className = "contact__rel";
-    elContactTel.className = "btn btn-success";
-    elContactTel.href = `tel:${contactInfo.tel}`;
-    elContactTitle.innerHTML = contactInfo.Name + ' ' + contactInfo.LName;
-    elContactText.innerHTML = contactInfo.rel;
-    elContactTel.innerHTML = contactInfo.tel;
-    // append childs
-    elContact.appendChild(elContactTitle);
-    elContact.appendChild(elContactText);
-    elContact.appendChild(elContactTel);
-    elContactArea.appendChild(elContact);
-    // clearing inputs
-    elInputName.value = "";
-    elInputLName.value = "";
-    elInputRel.value = "";
-    elInputNumber.value = "";
+    arr.push(obj);
+    document.querySelectorAll('.form-control').forEach(inp => {inp.value = ''})
+    if(isSame != true){
+      render(arr);
+    }else{
+      alert("bunday raqam mavjud")
+    }
   }
 }
-elBtnFamily.addEventListener("click" , ()=>{
-  elContactArea.innerHTML = null;
-  contactArr.forEach( function(cInfo) {
-    if(cInfo.rel == "Family"){
-      let elContactItem = document.createElement("div");
-      let elContacttitle = document.createElement("h4");
-      let elText = document.createElement("p");
-      let elTel = document.createElement("a");
-      elContactItem.className = "contact p-3 border border-start rounded";
-      elContacttitle.className = "contact__title";
-      elText.className = "contact__rel";
-      elTel.className = "btn btn-success";
-      elTel.href = `tel:${cInfo.tel}`;
-      elContacttitle.innerHTML = cInfo.Name + ' ' + cInfo.LName;
-      elText.innerHTML = cInfo.rel;
-      elTel.innerHTML = cInfo.tel;
-      elContactItem.appendChild(elContacttitle);
-      elContactItem.appendChild(elText);
-      elContactItem.appendChild(elTel);
-      elContactArea.appendChild(elContactItem);
+  let isSame = false;
+elInputNumber.addEventListener("keyup",()=>{
+  arr.forEach((item) => {
+    if(item.num == elInputNumber.value){
+      isSame = true;
+    }else{
+      isSame = false;
     }
-  })
-});
-elBtnFriends.addEventListener("click" , ()=>{
-  elContactArea.innerHTML = null;
-  contactArr.forEach( function(cInfo) {
-    if(cInfo.rel == "Friend"){
-      let elContactItem = document.createElement("div");
-      let elContacttitle = document.createElement("h4");
-      let elText = document.createElement("p");
-      let elTel = document.createElement("a");
-      elContactItem.className = "contact p-3 border border-start rounded";
-      elContacttitle.className = "contact__title";
-      elText.className = "contact__rel";
-      elTel.className = "btn btn-success";
-      elTel.href = `tel:${cInfo.tel}`;
-      elContacttitle.innerHTML = cInfo.Name + ' ' + cInfo.LName;
-      elText.innerHTML = cInfo.rel;
-      elTel.innerHTML = cInfo.tel;
-      elContactItem.appendChild(elContacttitle);
-      elContactItem.appendChild(elText);
-      elContactItem.appendChild(elTel);
-      elContactArea.appendChild(elContactItem);
-    }
-  })
-});
-elBtnClassm.addEventListener("click" , ()=>{
-  elContactArea.innerHTML = null;
-  contactArr.forEach( function(cInfo) {
-    if(cInfo.rel == "Classmate"){
-      let elContactItem = document.createElement("div");
-      let elContacttitle = document.createElement("h4");
-      let elText = document.createElement("p");
-      let elTel = document.createElement("a");
-      elContactItem.className = "contact p-3 border border-start rounded";
-      elContacttitle.className = "contact__title";
-      elText.className = "contact__rel";
-      elTel.className = "btn btn-success";
-      elTel.href = `tel:${cInfo.tel}`;
-      elContacttitle.innerHTML = cInfo.Name + ' ' + cInfo.LName;
-      elText.innerHTML = cInfo.rel;
-      elTel.innerHTML = cInfo.tel;
-      elContactItem.appendChild(elContacttitle);
-      elContactItem.appendChild(elText);
-      elContactItem.appendChild(elTel);
-      elContactArea.appendChild(elContactItem);
-    }
-  })
-});
-elBtnAll.addEventListener("click", ()=>{
-  elContactArea.innerHTML = null;
-  contactArr.forEach( function(cInfo) {
-    let elContactItem = document.createElement("div");
-    let elContacttitle = document.createElement("h4");
-    let elText = document.createElement("p");
-    let elTel = document.createElement("a");
-    elContactItem.className = "contact p-3 border border-start rounded";
-    elContacttitle.className = "contact__title";
-    elText.className = "contact__rel";
-    elTel.className = "btn btn-success";
-    elTel.href = `tel:${cInfo.tel}`;
-    elContacttitle.innerHTML = cInfo.Name + ' ' + cInfo.LName;
-    elText.innerHTML = cInfo.rel;
-    elTel.innerHTML = cInfo.tel;
-    elContactItem.appendChild(elContacttitle);
-    elContactItem.appendChild(elText);
-    elContactItem.appendChild(elTel);
-    elContactArea.appendChild(elContactItem);
-  })
+  });
 })
+function render(array) {
+  elContactArea.innerHTML = ""
+
+  array.forEach(item => {
+     let newTemp = template.cloneNode(true);
+     newTemp.querySelector(".contact__title").textContent = item.fullName;
+     newTemp.querySelector(".contact__rel").textContent = item.rel;
+     newTemp.querySelector(".tel-btn").textContent = item.num;
+     newTemp.querySelector(".tel-btn").href = `tel:${item.num}`;
+     elContactArea.append(newTemp);
+  });
+}
+elBtnAll.addEventListener('click', function () {
+  render(arr)
+});
+
+elBtnClassm.addEventListener('click', function () {
+  let filter = arr.filter(item => item.rel == "Classmate")
+  render(filter)
+});
+
+elBtnFamily.addEventListener('click', function () {
+  let filter = arr.filter(item => item.rel == "Family")
+  render(filter)
+});
+
+elBtnFriends.addEventListener('click', function () {
+  let filter = arr.filter(item => item.rel == "Friend")
+  render(filter)
+});
